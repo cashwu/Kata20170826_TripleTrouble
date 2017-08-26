3400a5db339bb39929a136db221fc4a262f073ff
cash <cash@cashwu.com>
2017-08-26 15:46:43 +0800

    input 111, 113 test case

--- Kata20170826_TripleTrouble/TripleTroubleTests.cs
+++ Kata20170826_TripleTrouble/TripleTroubleTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170826_TripleTrouble
@@ -12,6 +13,13 @@ namespace Kata20170826_TripleTrouble
             TripleDoubleShouldBe(0, 123, 123);
         }
 
+        [TestMethod]
+        public void input_111_113_should_return_1()
+        {
+            TripleDoubleShouldBe(1, 111, 113);
+        }
+
+
         private static void TripleDoubleShouldBe(int expected, int num1, int num2)
         {
             var kata = new Kata();
@@ -24,7 +32,9 @@ namespace Kata20170826_TripleTrouble
     {
         public int TripleDouble(long num1, long num2)
         {
-            return 0;
+            var num1IsTriple = num1.ToString().Count(a => char.GetNumericValue(a) == 1) == 3;
+            var num2IsDouble = num2.ToString().Count(a => char.GetNumericValue(a) == 1) == 2;
+            return num1IsTriple && num2IsDouble ? 1 : 0;
         }
     }
 }
