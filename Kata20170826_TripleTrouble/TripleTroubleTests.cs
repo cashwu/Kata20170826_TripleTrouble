d1709cf36c69a10800f574e3a92423a215392b57
cash <cash@cashwu.com>
2017-08-26 15:23:48 +0800

    input 123, 123 test case

--- Kata20170826_TripleTrouble/TripleTroubleTests.cs
+++ Kata20170826_TripleTrouble/TripleTroubleTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170826_TripleTrouble
     public class TripleTroubleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_123_123_should_return_0()
         {
+            TripleDoubleShouldBe(0, 123, 123);
+        }
+
+        private static void TripleDoubleShouldBe(int expected, int num1, int num2)
+        {
+            var kata = new Kata();
+            var actual = kata.TripleDouble(num1, num2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int TripleDouble(long num1, long num2)
+        {
+            return 0;
         }
     }
 }
